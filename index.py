9134811c00654ffe2e234c3d387cb2b20aa6af7c
Md. Al Amin <alamin.ece.ku@gmail.com>
2020-03-30 15:36:29 +0600

    Fix missing code

--- index.py
+++ index.py
@@ -1,1 +1,21 @@
-print("Hello")
+import json
+
+with open('./sample.json') as f:
+    data = json.load(f)
+
+print(type(data))
+
+page_keys = sorted(list(data.keys()))
+
+result = {}
+
+for page_key in page_keys:
+    group_keys = list(data[page_key].keys())
+
+    for group_key in group_keys:
+        new_data = data[page_key][group_key]
+
+        if group_key in result:
+            result[group_key] = result[group_key] + new_data
+        else:
+            result[group_key] = new_data
